510c41f9bc2d0797ec21a5f492ba15fd5e5c3509
Akanksha <akunagpal99@gmail.com>
2021-02-02 20:09:54 +0530

    home options

--- Lec7_Excel_Clone/activity/script.js
+++ Lec7_Excel_Clone/activity/script.js
@@ -1,4 +1,6 @@
 const $ = require("jquery");
+const dialog = require("electron").remote.dialog;
+const fs = require("fs");
 
 $(function () {
   
@@ -11,6 +13,143 @@ $(function () {
   // console.log("document is loaded !!!");
   // cell pe lagao click event and run function
 
+// new // open // save
+  $(".new").on("click" , function(){
+    // window.open();
+    console.log("new clicked !!");
+    // clean UI
+    clearUI();
+     // empty sheets
+     $(".sheets-list").html("");
+    // sheetsDb empty
+    sheetsDB = [];
+    // sheetId init
+    sheetId = 0;
+    // add a single db object to sheetsDB
+    addDBToSheetsDB();
+    // add a single Sheet to sheet list
+    addSheetToSheetList();
+  })
+  $(".open").on("click" , function(){
+    //open the dialog box
+    let paths = dialog.showOpenDialogSync();
+    console.log(paths);
+    if(paths){
+      let path = paths[0];
+      // clear the UI
+      clearUI();
+      // empty sheets
+      $(".sheets-list").html("");
+
+      let openedSheetsDB = JSON.parse(fs.readFileSync(path));
+      console.log(openedSheetsDB);
+
+      // sheetsDB set , db set , visited cells set , sheetId set
+      sheetsDB = openedSheetsDB;
+      db = sheetsDB[0].db;
+      currentVisitedCells = sheetsDB[0].visitedCells;
+      sheetId = 0;
+
+      for(let i=0 ; i<sheetsDB.length ; i++){
+        addSheetToSheetList();
+      }
+      // setUI according to current DB
+      setUI();
+    }
+
+
+
+
+  })
+  $(".save").on("click" , function(){
+    console.log("save clicked !!");
+    let path = dialog.showSaveDialogSync();
+    console.log(path);
+    if(path){
+      fs.writeFileSync(path , JSON.stringify(sheetsDB) );
+      alert("Sheets Saved Succesfully !");
+    }
+  })
+
+
+  // bold // underline // italic
+
+  $(".font-style div").on("click" , function(){
+    if(lsc){
+      let fontStyle = $(this).attr("class");
+      console.log(fontStyle);
+  
+      let rowId = $(lsc).attr("rowid");
+      let colId = $(lsc).attr("colid");
+  
+      let cellObject = db[rowId][colId];
+      
+  
+      if(fontStyle == "underline"){
+        // toggle => underline => normal => underline
+        $(lsc).css("text-decoration", cellObject.fontStyles.underline ? "none" : "underline" );
+      }
+      else if(fontStyle == "italic"){
+        $(lsc).css("font-style" , cellObject.fontStyles.italic ? "normal"  : "italic");
+      }
+      else{
+        $(lsc).css("font-weight" , cellObject.fontStyles.bold ? "normal" : "bold");
+      }
+      
+      // db changes
+      cellObject.fontStyles[fontStyle] = !cellObject.fontStyles[fontStyle];
+      
+      // cellObject.fontStyles[fontStyle] ? $(this).addClass("active-font-style") :$(this).removeClass("active-font-style") 
+    }
+  })
+
+
+  // left // centre // right
+  $(".right div").on("click" , function(){
+    if(lsc){
+      let rowId = $(lsc).attr("rowid");
+      let colId = $(lsc).attr("colid");
+      let cellObject = db[rowId][colId];
+      
+      let alignment = $(this).text();
+      if(alignment == "L"){
+        $(lsc).css("text-align" , "left");
+        cellObject.textAlign = "left";
+      }
+      else if(alignment == "C"){
+        $(lsc).css("text-align" , "center");
+        cellObject.textAlign = "center";
+      }
+      else{
+        $(lsc).css("text-align" , "right");
+        cellObject.textAlign = "right";
+      }
+    }
+  })
+
+  // cell color // font color
+  $("#cell-color").on("change" , function(){
+    let cellColor = $(this).val();
+    if(lsc){
+      let rowId = $(lsc).attr("rowid");
+      let colId = $(lsc).attr("colid");
+      let cellObject = db[rowId][colId];
+      $(lsc).css("background" , cellColor);
+      cellObject.color.cellColor = cellColor;
+    }
+  })
+  $("#font-color").on("change" , function(){
+    let fontColor = $(this).val();
+    if(lsc){
+      let rowId = $(lsc).attr("rowid");
+      let colId = $(lsc).attr("colid");
+      let cellObject = db[rowId][colId];
+      $(lsc).css("color" , fontColor);
+      cellObject.color.fontColor = fontColor;
+    }
+  })
+
+
   $(".cell").on("click", function () {
     console.log("cell clicked !!!!");
     let rowId = Number($(this).attr("rowid")); // 1 => 2
@@ -22,36 +161,49 @@ $(function () {
     // console.log(db);
   });
 
+  function addSheetToSheetList(){
+     // dynamically html add to dom
+     sheetId++;
+     // 2
+
+     let sheetToBeAdded;
+     if(sheetId == 1){
+      sheetToBeAdded = `<div class="sheet active-sheet" sid="${
+         sheetId - 1
+       }">Sheet ${sheetId}</div>`;
+     }
+     else{
+      sheetToBeAdded = `<div class="sheet" sid="${
+        sheetId - 1
+      }">Sheet ${sheetId}</div>`;
+     }
+
+     $(".sheets-list").append(sheetToBeAdded);
+     // 2 element dom .sheet class
+     // console.log($(`div[sid=${sheetId-1}]`));
+     $(`div[sid=${sheetId - 1}]`).on("click", function () {
+       // console.log("click event added on new sheet !!");
+       let isActive = $(this).hasClass("active-sheet");
+       if (!isActive) {
+         // remove active-sheet
+         $(".active-sheet").removeClass("active-sheet");
+         // add active-sheet to this
+         $(this).addClass("active-sheet");
+ 
+         // clear ui
+         clearUI();
+         // set db
+         let sid = $(this).attr("sid");
+         setDB(sid);
+         // set ui
+         setUI();
+       }
+     });
+  }
+
   // sheets logic
   $(".add-sheet").on("click", function () {
-    // dynamically html add to dom
-    sheetId++;
-    // 2
-    let sheetToBeAdded = `<div class="sheet" sid="${
-      sheetId - 1
-    }">Sheet ${sheetId}</div>`;
-    $(".sheets-list").append(sheetToBeAdded);
-    // 2 element dom .sheet class
-    // console.log($(`div[sid=${sheetId-1}]`));
-    $(`div[sid=${sheetId - 1}]`).on("click", function () {
-      // console.log("click event added on new sheet !!");
-      let isActive = $(this).hasClass("active-sheet");
-      if (!isActive) {
-        // remove active-sheet
-        $(".active-sheet").removeClass("active-sheet");
-        // add active-sheet to this
-        $(this).addClass("active-sheet");
-
-        // clear ui
-        clearUI();
-        // set db
-        let sid = $(this).attr("sid");
-        setDB(sid);
-        // set ui
-        setUI();
-      }
-    });
-
+    addSheetToSheetList();
     // new db add in sheetsDB
     addDBToSheetsDB();
   });
@@ -60,7 +212,8 @@ $(function () {
     for (let i = 0; i < currentVisitedCells.length; i++) {
       let rowId = currentVisitedCells[i].rowId;
       let colId = currentVisitedCells[i].colId;
-      $(`div[rowid=${rowId}][colid=${colId}]`).text("");
+      $(`div[rowid=${rowId}][colid=${colId}]`).html("");
+      $(`div[rowid=${rowId}][colid=${colId}]`).removeAttr("style");
     }
   }
   function setDB(sid) {
@@ -75,6 +228,7 @@ $(function () {
       let rowId = currentVisitedCells[i].rowId;
       let colId = currentVisitedCells[i].colId;
       let cellObject = db[rowId][colId];
+      // cellCOlor , fontColor , alignment , bold , underline , italic
       $(`div[rowid=${rowId}][colid=${colId}]`).text(cellObject.value);
     }
   }
@@ -284,6 +438,10 @@ $(function () {
           formula: "",
           childrens: [],
           parents: [],
+          visited: false,
+          fontStyles : {bold : false , italic : false , underline : false},
+          textAlign : "left" ,
+          color : {cellColor : "white" , fontColor : "black"}
         };
         row.push(cellObject);
       }
@@ -309,6 +467,9 @@ $(function () {
           childrens: [],
           parents: [],
           visited: false,
+          fontStyles : {bold : false , italic : false , underline : false},
+          textAlign : "left",
+          color : {cellColor : "white" , fontColor : "black"}
         };
         row.push(cellObject);
       }
