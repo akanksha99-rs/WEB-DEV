abd6dc26e73c9af1f7ee68045617e61098d9fea5
Akanksha <akunagpal99@gmail.com>
2021-03-21 15:56:13 +0530

    finalize page intro

--- Lec13_Resume_Builder/resume-builder/src/App.js
+++ Lec13_Resume_Builder/resume-builder/src/App.js
@@ -13,11 +13,21 @@ import Templates from "./Components/Templates/Templates";
 import Profile from "./Components/Profile/Profile"
 import SignUp from "./Components/SignUp/Signup";
 import Contact from "./Components/Contact/Contact";
+import Education from "./Components/Education/Education";
+import Finalize from "./Components/Finalize/Finalize";
 class App extends Component {
   state = {
     isAuth : false,
-    user : null
+    user : null ,
+    selectResumeId : null 
   };
+
+
+  setResumeId = (id)=>{
+    this.setState({
+      selectResumeId : id
+    })
+  }
 
 
   logout = () =>{
@@ -40,11 +50,25 @@ class App extends Component {
 
   componentDidMount(){
     // event attached to auth state changed
-    firebaseApp.auth().onAuthStateChanged( (user) =>{
+    firebaseApp.auth().onAuthStateChanged( async (user) =>{
       console.log("Inside auth state changed !!");
+      let selectResumeId = null;
+      // check if logged in ??
+      if(user){
+        // get selected resumeId
+        let doc = await firebaseApp.firestore().collection("users").doc(user.uid).get();
+        let resumes = doc.data()["Resumes"];
+        for(let i=0 ; i<resumes.length ; i++){
+          if(resumes[i].isSelected){
+            selectResumeId = resumes[i].resumeId;
+            break;
+          }
+        }
+      }
         this.setState({
           isAuth : user ? true : false ,
-          user : user ? user.uid : null 
+          user : user ? user.uid : null ,
+          selectResumeId : selectResumeId
         })
     })
   
@@ -67,12 +91,15 @@ class App extends Component {
             {/* <Skin></Skin> */}
           </Route>
 
-          <Route path="/contact" exact>
-            <Contact></Contact>
-          </Route>
-          
-          <Route path="/templates" exact component={Templates}></Route>
-            {/* {isAuth ? <Templates> </Templates> : <Redirect to="/login"></Redirect>  } */}          
+          <Route path="/contact" exact render={  (props) => this.state.isAuth ? <Contact {...props} uid={this.state.user} resumeId={this.state.selectResumeId} ></Contact> : <Redirect to="/signin"></Redirect>  }></Route>
+          <Route path="/education" exact render={  (props) => this.state.isAuth ? <Education {...props} uid={this.state.user} resumeId={this.state.selectResumeId} ></Education> : <Redirect to="/signin"></Redirect>  }></Route>
+          <Route path="/finalize" exact render={  (props) => this.state.isAuth ? <Finalize {...props} uid={this.state.user} resumeId={this.state.selectResumeId} ></Finalize> : <Redirect to="/signin"></Redirect>  }></Route>
+          {/* <Route path="/templates" exact component={Templates}></Route> */}
+          {/* {isAuth ? <Templates> </Templates> : <Redirect to="/login"></Redirect>  } */}     
+
+
+            <Route path="/templates" exact render = {   (props) => this.state.isAuth ? <Templates {...props}  uid = {this.state.user} resumeId={this.state.selectResumeId} setResumeId={this.setResumeId}></Templates> : <Redirect to="/signin"></Redirect>  }></Route>
+
           
           <Route path="/profile" exact>
             {isAuth ? <Profile></Profile> : <Redirect to="/login"></Redirect>}
--- Lec13_Resume_Builder/resume-builder/src/Components/Skins/skin1.jsx
+++ Lec13_Resume_Builder/resume-builder/src/Components/Skins/skin1.jsx
@@ -1,112 +1,164 @@
 import React, { Component } from "react";
-import firebaseApp from "../../firebase/firebaseConfig";
-
-
-class SignUp extends Component {
+import "./skin1.css";
+class Skin extends Component {
   state = {
-    fname: "",
-    lname:"",
-    id: "",
-    pw: "",
-    error: "",
+    contactDetails: {
+      fname: "Akanksha",
+      lname: "Nagpal",
+      summary: "I am  a Btech student",
+      email: "akanksha_nagpal@outlook.com",
+      phone: "9899770701",
+      profession: "Engineer",
+      street: "IP Extension",
+      city: "Delhi",
+      state: "Delhi",
+      country: "India",
+      pin: "110092",
+    },
+    educationDetails: {
+      collegeName: "IGDTUW",
+      degree: "BTech",
+      cgpa: "9",
+      collegeCity: "Delhi",
+      collegeState: "Delhi",
+      graduationMonth: "May",
+      graduationYear: "2022",
+    },
+    experienceDetails: [
+      { companyName: "Microsoft", duration: "2 months", position: "intern" },
+      { companyName: "", duration: "", position: "" },
+      { companyName: "", duration: "", position: "" },
+    ],
+    projects: [
+      { projectName: "A", techStack: ["E", "F", "G"], summary: "abc" , projectLink:"" },
+      { projectName: "B", techStack: ["J", "I", "H"], summary: "abcd" , projectLink:""},
+      { projectName: "C", techStack: ["K", "L", "M"], summary: "abcde"  , projectLink:""},
+      { projectName: "D", techStack: ["P", "O", "N"], summary: "abcdef"  , projectLink:""}
+    ],
+    skills : {
+        language : ["C++" , "" , ""] ,
+        frameworks : ["" , "" , ""] ,
+        software : ["" , "" , ""] ,
+        ide : ["" , "" , ""]
+    } ,
+    profileLinks : {
+        linkedIn : "" ,
+        github : "" 
+    } ,
+    achievements : ["" ,"" ,"" , "" ,""] ,
+    hobbies : ["Reading" , "" , "" , "" , ""] 
   };
-
-  onChangeHandler = (e) => {
-    let id = e.target.id;
-    let value = e.target.value;
-    this.setState({
-      [id]: value,
-    });
-  };
-
-
-  signUpHandler = () =>{
-      // this.props.signup !!!
-      let id = this.state.id;
-      let pw = this.state.pw;
-      firebaseApp.auth().createUserWithEmailAndPassword(id , pw)
-      .then( userInfo =>{
-          console.log("Inside then");
-        //   console.log(userInfo);
-        let fname = this.state.fname;
-        let lname = this.state.lname;
-        let uid = userInfo.user.uid;
-        let userCreatedPromise = firebaseApp.firestore().collection("users").doc(uid).set({
-            "First Name" : fname ,
-            "Last Name": lname ,
-            "Email" : id ,
-            "Password":pw ,
-            "uid" : uid
-        })
-        return userCreatedPromise;
-      })
-      .then( obj =>{
-          console.log("User Created !!!!");
-          console.log(obj);
-      })
-      .catch(error=>{
-          console.log("inside catch");
-          console.log(error);
-          this.setState({
-              error : error.message
-          })
-      })
-      
-  }
-
   render() {
+    let {
+      fname,
+      lname,
+      summary,
+      email,
+      phone,
+      profession,
+      street,
+      city,
+      state,
+      country,
+      pin,
+    } = this.state.contactDetails;
+    let {
+      collegeName,
+      degree,
+      cgpa,
+      collegeCity,
+      collegeState,
+      graduationMonth,
+      graduationYear,
+    } = this.state.educationDetails;
+    let experienceDetails = this.state.experienceDetails;
+    let projects = this.state.projects;
+    let skills = this.state.skills;
+    let profileLinks = this.state.profileLinks;
+    let achievements = this.state.achievements;
+    let hobbies = this.state.hobbies;
+    
     return (
-      <div className="signup">
-        <div>
-          <h2>First Name</h2>
-          <input
-            type="text"
-            id="fname"
-            value={this.state.fname}
-            onChange={(e) => {
-              this.onChangeHandler(e);
-            }}
-          />
-        </div>  
-        <div>
-          <h2>Last Name</h2>
-          <input
-            type="text"
-            id="lname"
-            value={this.state.lname}
-            onChange={(e) => {
-              this.onChangeHandler(e);
-            }}
-          />
-        </div>  
-        <div>
-          <h2>Id</h2>
-          <input
-            type="text"
-            id="id"
-            value={this.state.id}
-            onChange={(e) => {
-              this.onChangeHandler(e);
-            }}
-          />
-        </div>
-        <div>
-          <h2>Password</h2>
-          <input
-            type="text"
-            id="pw"
-            value={this.state.pw}
-            onChange={(e) => {
-              this.onChangeHandler(e);
-            }}
-          />
-        </div>
-           <button className="btn" onClick={ this.signUpHandler }>Sign Up</button> 
+      <div class="resume-skin">
+        <div className="resume-left-strap">
 
-           <p>{this.state.error}</p>
+        <div className="contact-details-skin1">
+        <div className="contact-full-name">
+          {fname} {lname}
+        </div>
+
+        <div className="email-phone">
+          <div className="email-details">{email}</div>
+          <div className="phone-details">{phone}</div>
+        </div>
+        
+        <div className="address-details">
+          {street} {city} {pin} {state} {country}
+        </div>
+        <div className="profession-details">{profession}</div>
       </div>
+
+      <div className="professional-summary-skin1">
+        <div className="professional-summary-heading">Summary</div>
+        <div className="professional-summary-details">{summary}</div>
+      </div>
+
+      <div className="resume-body">
+  <div className="education-details-skin1">
+        <div className="education-details-heading">Education</div>
+        <div className="education-details">
+          <div className="education-details-degree">
+            {degree} with GPA : {cgpa}
+          </div>
+
+          <div className="education-details-name">
+            {collegeName}  ,  {graduationMonth}{" "}
+            {graduationYear}
+          </div>
+        </div>
+      </div>
+
+  </div>
+
+
+
+      <div className="Skills-details-skin1">
+        <div className="skills-details-heading">Skills</div>
+        <div className="skills-details">
+          <ul><li>C++</li>
+          <li>Java</li>
+          <li>JavaScript</li>
+          </ul>
+        </div>
+      </div> 
+
+      <div className="projects-details-skin1">
+        <div className="projects-details-heading">Projects</div>
+        <div className="projects-details">
+          <ul><li>project 1</li>
+          <li>project 2</li>
+          <li>project 3</li>
+          </ul>
+        </div>
+      </div> 
+
+      
+  
+  </div>
+  <div className="resume-header">
+    <div className="user-Details">
+      <div className="user-information">
+       <div className="div-line"></div>
+        
+      </div>
+      
+    </div>
+
+  </div>
+
+  
+</div>
     );
   }
 }
-
-export default SignUp;
+export default Skin;
