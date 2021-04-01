eec9b87b32ece06e6d894b58c79a0fb573cf2a4e
Akanksha <akunagpal99@gmail.com>
2021-04-01 21:56:18 +0530

    pdf update

--- Lec13_Resume_Builder/resume-builder/src/Components/Contact/Contact.jsx
+++ Lec13_Resume_Builder/resume-builder/src/Components/Contact/Contact.jsx
@@ -4,7 +4,6 @@ import { contactCodes } from "../../util/codes";
 import Skin1 from "../Skins/skin1";
 import "./Contact.css";
 
-
 class Contact extends Component {
   state = {
     codes: [
@@ -33,52 +32,92 @@ class Contact extends Component {
       country: "",
       pin: "",
     },
-    skinId:null
+    skinId: null,
   };
 
   onChangeHandler = (e) => {
     // console.log(contactCodes);
-
     let id = e.target.id;
     let value = e.target.value;
     let oldContactDetails = this.state.contactDetails;
     this.setState({
-        contactDetails:{
-            ...oldContactDetails ,
-            [id]:value
-        }
+      contactDetails: {
+        ...oldContactDetails,
+        [id]: value,
+      },
     });
   };
 
-
-  componentDidMount(){
-    // get contactDetails of the selected Resume !!!
-    firebaseApp.firestore().collection("resumes").doc(this.props.resumeId).get().then( doc =>{
-      console.log("Inside component did mount of contact !!!");
-      let {contactDetails , skinId} = doc.data();
-      
-      // console.log(contactDetails);
-      // console.log(skinId);
+  componentDidMount() {
+    // if coming back from education page
+    if (this.props.location.state.contactDetails) {
       this.setState({
-        contactDetails : contactDetails ,
-        skinId : skinId
-      })
-    })
+        contactDetails: this.props.location.state.contactDetails,
+        skinId: this.props.location.state.skinId,
+      });
+    } else if (this.props.location.state.resumeId) {
+      console.log("Coming from my resumes !!!");
+      // if coming from myResumes
+      // contactDetails
+      // skinId
+      // selectedResumeId
+      this.setState({
+        contactDetails: this.props.location.state.resumeDetails.contactDetails,
+        skinId: this.props.location.state.resumeDetails.skinId,
+      });
+    } else {
+      // if coming from templates page
+      this.setState({
+        skinId: this.props.location.state.skinId,
+      });
+    }
+
+    // get contactDetails of the selected Resume !!!
+    // firebaseApp.firestore().collection("resumes").doc(this.props.resumeId).get().then( doc =>{
+    //   console.log("Inside component did mount of contact !!!");
+    //   let {contactDetails , skinId} = doc.data();
+
+    //   // console.log(contactDetails);
+    //   // console.log(skinId);
+    //   this.setState({
+    //     contactDetails : contactDetails ,
+    //     skinId : skinId
+    //   })
+    // })
   }
 
-  nextButtonHandler = async ()=>{
+  nextButtonHandler = async () => {
     console.log("next button clicked !!");
     // save contactDetails object in doc
-    await firebaseApp.firestore().collection("resumes").doc(this.props.resumeId).update({
-      contactDetails : this.state.contactDetails
-    });
-    this.props.history.push("/education");
-  }
+    // await firebaseApp.firestore().collection("resumes").doc(this.props.resumeId).update({
+    //   contactDetails : this.state.contactDetails
+    // });
+    if(this.props.location.state.resumeId){
+      this.props.history.push({
+        pathname: "/education",
+        state: {
+          contactDetails: this.state.contactDetails,
+          skinId: this.state.skinId,
+          originalDetails : this.props.location.state.resumeDetails ,
+          resumeId : this.props.location.state.resumeId
+        },
+      });
+    }
+    else{
+          this.props.history.push({
+            pathname: "/education",
+            state: {
+              contactDetails: this.state.contactDetails,
+              skinId: this.state.skinId,
+            },
+          });
+    }
+  };
 
-  backButtonHandler = ()=>{
+  backButtonHandler = () => {
     console.log("back button clicked !!");
     this.props.history.goBack();
-  }
+  };
 
   render() {
     return (
@@ -99,13 +138,17 @@ class Contact extends Component {
               </div>
             );
           })}
-          <button className="btn" onClick={this.nextButtonHandler}>Next</button>
-          <button className="btn" onClick={this.backButtonHandler}>Back</button>
+          <button className="btn" onClick={this.nextButtonHandler}>
+            Next
+          </button>
+          <button className="btn" onClick={this.backButtonHandler}>
+            Back
+          </button>
         </div>
         <div className="resume-viewer">
           {/* get skin according to skinId */}
           {/* <Skin skinId={this.state.skinId} contactDetails = {this.state.contactDetails} ></Skin> */}
-          <Skin1 contactDetails = {this.state.contactDetails}></Skin1>
+          <Skin1 contactDetails={this.state.contactDetails}></Skin1>
         </div>
       </div>
     );
--- Lec13_Resume_Builder/resume-builder/src/Components/Skins/skin2.jsx
+++ Lec13_Resume_Builder/resume-builder/src/Components/Skins/skin2.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import "./skin2.css";
+import "./skin1.css";
 class Skin1 extends Component {
   state = {
     contactDetails: {
@@ -127,7 +127,7 @@ class Skin1 extends Component {
     let achievements = this.state.achievements;
     let hobbies = this.state.hobbies;
     return (
-      <div className="resume-skin">
+      <div ref={this.props.refProp} className="resume-skin">
         <div className="skin-top">
           <div className="user-heading">
             <div className="user-name">
